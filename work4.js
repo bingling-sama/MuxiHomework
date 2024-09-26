f3a1b60a276c05c809342a2b1f25991d92224c1d
bingling-sama <bingling_sama@qq.com>
2024-09-26 13:44:55 +0800

    ✨ feat: homework 4

---
+++ work4.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function (arr, fn) {
+	return arr.sort(fn)
+};
+
+console.log(sortBy([5, 4, 1, 2, 3], x => x))
+console.log(sortBy([{ x: 1 }, { x: 0 }, { x: -1 }], (d) => d.x))
+console.log(sortBy([[3, 4], [5, 2], [10, 1]], x => x[1]))
